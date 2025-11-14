4053109a2fd32f2d9f0d26162ae770453707c603
Mosè Giordano <mose@gnu.org>
2025-11-14 18:31:22 +0000

    Also add command to deploy the docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,3 +8,12 @@ makedocs(
         "Home" => "index.md",
     ],
 )
+
+deploydocs(
+    ;
+    repo = "github.com/pvermees/KJ.jl",
+    devbranch = "main",
+    # Only push previews if all the relevant environment variables are non-empty. This is an
+    # attempt to work around https://github.com/JuliaDocs/Documenter.jl/issues/2048.
+    push_preview = all(!isempty, (get(ENV, "GITHUB_TOKEN", ""), get(ENV, "DOCUMENTER_KEY", ""))),
+)
