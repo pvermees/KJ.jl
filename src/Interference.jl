acc4523d126fa2f8adbaabdbf7bf753ef4898b75
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2026-02-12 17:29:55 +0000

    Removed Interference.jl

--- src/Interference.jl
+++
@@ -1,7 +0,0 @@
-import Base: ==, hash
-
-==(a::Interference, b::Interference) = all(getfield(a, f) == getfield(b, f) for f in fieldnames(Interference))
-==(a::REEInterference, b::REEInterference) = all(getfield(a, f) == getfield(b, f) for f in fieldnames(REEinterference))
-
-hash(a::Interference, h::UInt) = hash(a.ion, hash(a.proxy, hash(a.channel, h)))
-hash(a::REEInterference, h::UInt) = hash(a.proxy, hash(a.REE, hash(a.REEO, h)))
